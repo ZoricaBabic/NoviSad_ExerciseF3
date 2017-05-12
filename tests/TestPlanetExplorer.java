0d62c2bd30ca05fb6802b49611d222c3e52ee511
somename <someemail>
2017-05-12 20:36:12 +0200

    besouro automatic message

--- tests/TestPlanetExplorer.java
+++ tests/TestPlanetExplorer.java
@@ -3,9 +3,23 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestPlanetExplorer {
+	
+	PlanetExplorer explorer = new PlanetExplorer(3,3);
 
 	@Test
 	public void test_executeCommand() {
 		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void test_String(){
+		
+		
+		int x = 2;
+		int y = 3; 
+		
+		
+		String expected = "(" + x + "," + y + ")";
+		assertEquals(expected,explorer.toString(x,y));
 	}
 }
